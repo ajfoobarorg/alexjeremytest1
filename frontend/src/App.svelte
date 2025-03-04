6d2846272bb8f9b4a02e0e695016aad7ca5495d1
Jeremy Ginsberg <jeremyg@gmail.com>
2025-03-05 12:05:05 +1300

    Remove dead code which was trying to hit a non-existent endpoint

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -5,55 +5,6 @@
 
   $: path = $currentPath;
   $: gameId = path.startsWith('/game/') ? path.split('/game/')[1] : null;
-
-  let board = Array(9).fill("");
-  let currentPlayer = "X";
-  let winner = null;
-  let gameOver = false;
-
-  async function fetchGameState() {
-    const response = await fetch('http://localhost:8000/game');
-    const data = await response.json();
-    board = data.board;
-    currentPlayer = data.current_player;
-    winner = data.winner;
-    gameOver = data.game_over;
-  }
-
-  async function makeMove(position) {
-    if (board[position] || gameOver) return;
-
-    try {
-      const response = await fetch(`http://localhost:8000/move/${position}`, {
-        method: 'POST'
-      });
-      const data = await response.json();
-      board = data.board;
-      currentPlayer = data.current_player;
-      winner = data.winner;
-      gameOver = data.game_over;
-    } catch (error) {
-      console.error('Error making move:', error);
-    }
-  }
-
-  async function resetGame() {
-    try {
-      const response = await fetch('http://localhost:8000/reset', {
-        method: 'POST'
-      });
-      const data = await response.json();
-      board = data.board;
-      currentPlayer = data.current_player;
-      winner = null;
-      gameOver = false;
-    } catch (error) {
-      console.error('Error resetting game:', error);
-    }
-  }
-
-  // Initial game state
-  fetchGameState();
 </script>
 
 <main>
